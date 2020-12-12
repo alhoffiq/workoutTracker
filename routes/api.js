98458eade6426afe212683aed13a34a4f2c260d6
alhoff <andrew.hoff14@yahoo.com>
2020-12-12 16:03:23 -0700

    Fixed models, I think. Added remaining html routes. Cleaned up some edits on the front end

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,21 @@
 const router = require("express").Router();
-const Workout = require("../models/workout.js");
+const db = require("../models");
 const path = require('path');
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 router.get("/exercise", ({ body }, res) => {
   res.sendFile(path.join(__dirname + '/../public/exercise.html'));
 })
 
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
 router.post("/api/workouts", ({ body }, res) => {
-  Workout.create(body)
+  db.Workout.create(body)
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
@@ -16,8 +24,8 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
-router.put("/api/workouts/:id", ({ body }, res) => {
-  Workout.insertMany(body)
+router.put("/api/workouts/:id", (req, res) => {
+  db.Workout.findOneAndUpdate(req.params.id, { $push: { exercises: req.body } })
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
@@ -27,8 +35,7 @@ router.put("/api/workouts/:id", ({ body }, res) => {
 });
 
 router.get("/api/workouts", (req, res) => {
-  Workout.find({})
-    .sort()
+  db.Workout.find({})
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
